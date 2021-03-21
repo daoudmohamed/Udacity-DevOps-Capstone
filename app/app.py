87927a731253d4e5d3987895ee6ded7c9103ba19
MED <daoud.mohamed.tn@gmail.com>
2021-03-21 22:11:14 +0100

    Add simple flask app

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import os 
+
+app = Flask(__name__)
+
+@app.route('/')
+def print_env_build_number():
+	version = os.environ.get("BUILD_VERSION", default="DEV")
+	return f"Hello, World! <br> <b>Build version = <i>{version}</i></b>"
